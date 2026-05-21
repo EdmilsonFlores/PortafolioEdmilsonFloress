7801d02be4caae0d2bd1a2cab01e45f7cb27b366
EdmilsonFlores <edmilsonfloress@gmail.com>
2026-05-20 20:44:37 -0600

    .

---
+++ src/main/java/TechShop/Edmilson/EdmilsonApplication.java
@@ -0,0 +1,13 @@
+package TechShop.Edmilson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdmilsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdmilsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TechShop/Edmilson/EdmilsonApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop.Edmilson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdmilsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
