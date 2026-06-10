73b519143c130ab65fce4ce172a16230baf4667a
EdmilsonFlores <edmilsonfloress@gmail.com>
2026-06-10 17:20:11 -0600

    Semana 3
    Código presentación S3

---
+++ src/main/java/TechShop/Edmilson/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package TechShop.Edmilson.repository;
+
+/**
+ *
+ * @author edflo
+ */
+public interface CategoriaRepository {
+    
+}
